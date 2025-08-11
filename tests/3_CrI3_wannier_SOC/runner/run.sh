54d51e51daf42dd1d23d1cdc8be2a9543cfc657f
HeXu <mailhexu@gmail.com>
2025-08-11 23:49:14 +0200

    add test wannier with SOC

--- tests/3_CrI3_wannier_SOC/runner/run.sh
+++ tests/3_CrI3_wannier_SOC/runner/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Dummy run script for template test
-echo "Running dummy test"
-echo "# Dummy reference output for template test" > ../result/output1.dat
-echo "value = 42" >> ../result/output1.dat
+#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/x --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path refs/TB2J_results_x
+#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/y --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path refs/TB2J_results_y
+#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/z --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path refs/TB2J_results_z
+TB2J_merge.py -T structure  refs/TB2J_results_x refs/TB2J_results_y refs/TB2J_results_z --output_path refs/TB2J_results
