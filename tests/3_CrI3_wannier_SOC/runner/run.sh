22b9e47a0f38b6a00a13f9183c322e42e510b3a7
HeXu <mailhexu@gmail.com>
2025-08-11 23:51:17 +0200

    update run.sh with output dir

--- tests/3_CrI3_wannier_SOC/runner/run.sh
+++ tests/3_CrI3_wannier_SOC/runner/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/x --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path refs/TB2J_results_x
-#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/y --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path refs/TB2J_results_y
-#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/z --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path refs/TB2J_results_z
-TB2J_merge.py -T structure  refs/TB2J_results_x refs/TB2J_results_y refs/TB2J_results_z --output_path refs/TB2J_results
+#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/x --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path result/TB2J_results_x
+#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/y --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path result/TB2J_results_y
+#wann2J.py --elements Cr --efermi -0.87  --spinor --path ../../inputs/3_CrI3_wannier_SOC/data/z --prefix_spinor='wannier90' --kmesh 7 7 1  --nz 50 --groupby orbital --output_path result/TB2J_results_z
+TB2J_merge.py -T structure  refs/TB2J_results_x refs/TB2J_results_y refs/TB2J_results_z --output_path result/TB2J_results
